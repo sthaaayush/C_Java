7643b7c2cef1fc880ba7c30d47881b8ba74cf1fe
Aayush Shrestha <aayshstha20@gmail.com>
2022-07-23 15:19:43 +0545

    java

---
+++ JAVAWork.java
@@ -0,0 +1,16 @@
+package work;
+
+/**
+ *
+ * @author aayus
+ */
+public class JAVAWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
